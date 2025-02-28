e4b2b1d8f8a2179c5e375ce97a991067fd0f573b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-28 16:35:28 -0300

    feat: implements the testSoma and testSubtracao tests in the test class.

--- src/test/java/br/com/calculadora_java/CalculadoraJavaApplicationTests.java
+++ src/test/java/br/com/calculadora_java/CalculadoraJavaApplicationTests.java
@@ -1,13 +1,21 @@
 package br.com.calculadora_java;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class CalculadoraJavaApplicationTests {
 
+	private final CalculadoraJavaApplication calculadora = new CalculadoraJavaApplication();
+
 	@Test
-	void contextLoads() {
+	void testSoma() {
+		double resultado = calculadora.calcular(10.25, 5.75, 1);
+		assertEquals(16.00, resultado, 0.01, "O resultado da soma está incorreto!");
 	}
 
+	@Test
+	void testSubtracao() {
+		double resultado = calculadora.calcular(10.25, 5.75, 2);
+		assertEquals(4.50, resultado, 0.01, "O resultado da subtração está incorreto!");
+	}
 }
