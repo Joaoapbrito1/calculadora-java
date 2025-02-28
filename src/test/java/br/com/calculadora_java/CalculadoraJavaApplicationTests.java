9bbf888dfdf0524b0afa650bd9709cec1b1baccf
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-28 14:50:26 -0300

    refactor: integrate Spring Boot into project.

---
+++ src/test/java/br/com/calculadora_java/CalculadoraJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calculadora_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
