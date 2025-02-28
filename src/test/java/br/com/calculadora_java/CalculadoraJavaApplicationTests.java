8abdc393b3eec370d8f61ba79c49f5cf44d08a20
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-28 16:59:06 -0300

    feat: implements the testMultiplicacao, testDivisao, testDivisaoPorZer  and testOperacaoInvalida  tests in the test class.

--- src/test/java/br/com/calculadora_java/CalculadoraJavaApplicationTests.java
+++ src/test/java/br/com/calculadora_java/CalculadoraJavaApplicationTests.java
@@ -18,4 +18,32 @@ class CalculadoraJavaApplicationTests {
 		double resultado = calculadora.calcular(10.25, 5.75, 2);
 		assertEquals(4.50, resultado, 0.01, "O resultado da subtração está incorreto!");
 	}
+
+	@Test
+	void testMultiplicacao() {
+		double resultado = calculadora.calcular(4.35, 4.26, 3);
+		assertEquals(18.52, resultado, 0.02, "O resultado da multiplicação está incorreto!");
+	}
+
+	@Test
+	void testDivisao() {
+		double resultado = calculadora.calcular(10.0, 4.0, 4);
+		assertEquals(2.50, resultado, 0.01, "O resultado da divisão está incorreto!");
+	}
+
+	@Test
+	void testDivisaoPorZero() {
+		Exception exception = assertThrows(IllegalArgumentException.class, () ->
+				calculadora.calcular(10.0, 0.0, 4)
+		);
+		assertEquals("Não é possível dividir por zero.", exception.getMessage());
+	}
+
+	@Test
+	void testOperacaoInvalida() {
+		Exception exception = assertThrows(IllegalArgumentException.class, () ->
+				calculadora.calcular(10.0, 5.0, 99)
+		);
+		assertEquals("Operação inválida.", exception.getMessage());
+	}
 }
